e6e0c3696c5016927ae26aea4cd3c159ab9e9600
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2018-10-01 17:36:36 -0500

    Math, Probabilities

--- 0009A - Die Roll.java
+++ 0009A - Die Roll.java
@@ -25,7 +25,7 @@ to 1, output <<1/1>>.
 ------------------------------------------------------------------------------------------------------------------*/
 
 import java.util.Scanner;
-public class A009_DieRoll { 
+public class A0009_DieRoll { 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
