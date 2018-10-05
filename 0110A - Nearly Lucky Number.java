dbeb73e4aaa83faf72ca527aaf8cb56e8dcd13d2
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2018-10-04 22:52:24 -0500

    Implementation

--- 0110A - Nearly Lucky Number.java
+++ 0110A - Nearly Lucky Number.java
@@ -1,16 +1,15 @@
-package A;
 /*
+
+A - Nearly Lucky Number
 
 Petya loves lucky numbers. We all know that lucky numbers are the positive 
 integers whose decimal representations contain only the lucky digits 4 and 7. 
 For example, numbers 47, 744, 4 are lucky and 5, 17, 467 are not.
-
 Unfortunately, not all numbers are lucky. Petya calls a number nearly lucky 
 if the number of lucky digits in it is a lucky number. He wonders whether 
 number n is a nearly lucky number.
 
 Input
-
 The only line contains an integer n (1 <= n <= 10^18).
 Please do not use the %lld specificator to read or write 64-bit numbers in C++. 
 It is preferred to use the cin, cout streams or the %I64d specificator.
@@ -22,7 +21,7 @@ Print on the single line "YES" if n is a nearly lucky number. Otherwise, print
 ------------------------------------------------------------------------------*/
 
 import java.util.Scanner;
-public class A110_NearlyLuckyNumber { 
+public class A0110_NearlyLuckyNumber { 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
