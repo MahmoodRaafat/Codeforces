cf8aa1135524b89df6238486fa8074308b898f57
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2018-10-04 22:47:39 -0500

    Math, Implementation

--- 0082A - Double Cola.java
+++ 0082A - Double Cola.java
@@ -1,4 +1,3 @@
-package A;
 /*
 
 A - Double Cola
@@ -20,7 +19,7 @@ It is guaranteed that the pretests check the spelling of all the five names, tha
 that they contain all the five possible answers.
 
 Output
-Print the single line — the name of the person who drinks the n-th can of cola. The 
+Print the single line â€” the name of the person who drinks the n-th can of cola. The 
 cans are numbered starting from 1. Please note that you should spell the names like 
 this: "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" (without the quotes). In that 
 order precisely the friends are in the queue initially.
@@ -28,7 +27,7 @@ order precisely the friends are in the queue initially.
 ------------------------------------------------------------------------------------*/
 
 import java.util.Scanner;
-public class A082_DoubleCola {
+public class A0082_DoubleCola {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
