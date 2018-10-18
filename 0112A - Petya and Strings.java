d7346edaa9df8120314f6d2621601bc1fecdd8b3
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2018-10-18 03:03:59 -0500

    Strings, Implementation

--- 0112A - Petya and Strings.java
+++ 0112A - Petya and Strings.java
@@ -1,4 +1,3 @@
-package A;
 /*
 
 A - Petya and Strings
@@ -22,7 +21,7 @@ that the letters' case is not taken into consideration when the strings are comp
 --------------------------------------------------------------------------------------*/
 
 import java.util.Scanner;
-public class A112_PetyaAndStrings {
+public class A0112_PetyaAndStrings {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
