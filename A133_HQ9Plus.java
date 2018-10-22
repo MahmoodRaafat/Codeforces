f39acb9c779293b2b3bc0943db962a7c28ef4991
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2018-10-22 01:41:21 -0500

    Add files via upload

---
+++ A133_HQ9Plus.java
@@ -0,0 +1,38 @@
+package A;
+/*
+
+A - HQ9+
+
+HQ9+ is a joke programming language which has only four one-character instructions:
+    "H" prints "Hello, World!",
+    "Q" prints the source code of the program itself,
+    "9" prints the lyrics of "99 Bottles of Beer" song,
+    "+" increments the value stored in the internal accumulator.
+Instructions "H" and "Q" are case-sensitive and must be uppercase. The characters of the program 
+which are not instructions are ignored. You are given a program written in HQ9+. You have to figure 
+out whether executing this program will produce any output.
+
+Input
+The input will consist of a single line p which will give a program in HQ9+. String p will contain 
+between 1 and 100 characters, inclusive. ASCII-code of each character of p will be between 33 
+(exclamation mark) and 126 (tilde), inclusive.
+
+Output
+Output "YES", if executing the program will produce any output, and "NO" otherwise.
+
+--------------------------------------------------------------------------------------------------*/
+
+import java.util.Scanner;
+public class A133_HQ9Plus {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		sc.close();
+		
+		if (word.contains("H") || word.contains("Q") || word.contains("9"))
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+	}
+}
---
+++ A141_AmusingJoke.java
@@ -0,0 +1,76 @@
+/*
+
+A - Amusing Joke
+
+So, the New Year holidays are over. Santa Claus and his colleagues can take a rest and 
+have guests at last. When two "New Year and Christmas Men" meet, thear assistants cut 
+out of cardboard the letters from the guest's name and the host's name in honor of this 
+event. Then the hung the letters above the main entrance. One night, when everyone went 
+to bed, someone took all the letters of our characters' names. Then he may have shuffled 
+the letters and put them in one pile in front of the door.
+The next morning it was impossible to find the culprit who had made the disorder. But 
+everybody wondered whether it is possible to restore the names of the host and his 
+guests from the letters lying at the door? That is, we need to verify that there are no 
+extra letters, and that nobody will need to cut more letters.
+Help the "New Year and Christmas Men" and their friends to cope with this problem. You 
+are given both inscriptions that hung over the front door the previous night, and a pile 
+of letters that were found at the front door next morning.
+
+Input
+The input file consists of three lines: the first line contains the guest's name, the 
+second line contains the name of the residence host and the third line contains letters 
+in a pile that were found at the door in the morning. All lines are not empty and contain 
+only uppercase Latin letters. The length of each line does not exceed 100.
+
+Output
+Print "YES" without the quotes, if the letters in the pile could be permuted to make the 
+names of the "New Year and Christmas Men". Otherwise, print "NO" without the quotes.
+
+----------------------------------------------------------------------------------------*/
+
+import java.util.Scanner;
+public class A141_AmusingJoke {
+	static String pile;
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		String str2 = sc.next();
+		pile = sc.next();
+		sc.close();
+		
+		// if pile contains all letters from 2 strings and nothing is left
+		if (hasLetters(str1) && hasLetters(str2) && pile.equals(""))
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+	}
+	
+	// determines if all the letters of a string are in the pile
+	public static boolean hasLetters(String str) {
+		boolean[] used = new boolean[pile.length()];
+		for (int i=0; i<str.length(); i++) {
+			boolean hasLetter = false;
+			for (int j=0; j<pile.length(); j++) {
+				if (str.charAt(i)==pile.charAt(j) && !used[j]) {
+					hasLetter = true;
+					used[j] = true;
+					break;
+				}
+			}
+			if (!hasLetter)
+				return false;
+		}
+		removeLetters(used);
+		return true;
+	}
+	
+	// if so, remove them from pile
+	public static void removeLetters(boolean[] used) {
+		String res = "";
+		for (int i=0; i<pile.length(); i++)
+			if (!used[i])
+				res += pile.charAt(i);
+		pile = res;
+	}
+}
