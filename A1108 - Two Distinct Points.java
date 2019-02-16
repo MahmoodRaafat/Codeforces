63739616ed6994de4516bdb9cf379fb8e9758530
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2019-02-15 22:34:20 -0600

    Implementation

--- A1108 - Two Distinct Points.java
+++ A1108 - Two Distinct Points.java
@@ -10,12 +10,12 @@ the segment [l2 to r2]. It is guaranteed that the answer exists. If there are mu
 print any of them. You have to answer q independent queries.
 
 Input
-The first line of the input contains one integer q (1<=q<=500) — the number of queries. Each of the next q
-lines contains four integers l1i, r1i, l2i and r2i (1<=l1i, r1i, l2i, r2i<=109, l1i<r1i, l2i<r2i) — the ends 
+The first line of the input contains one integer q (1<=q<=500) â€” the number of queries. Each of the next q
+lines contains four integers l1i, r1i, l2i and r2i (1<=l1i, r1i, l2i, r2i<=109, l1i<r1i, l2i<r2i) â€” the ends 
 of the segments in the i-th query.
 
 Output
-Print 2q integers. For the i-th query print two integers ai and bi — such numbers that l1i<=ai<=r1i, 
+Print 2q integers. For the i-th query print two integers ai and bi â€” such numbers that l1i<=ai<=r1i, 
 l2i<=bi<=r2i and ai!=bi. Queries are numbered in order of the input. It is guaranteed that the answer exists. 
 If there are multiple answers, you can print any.
 
