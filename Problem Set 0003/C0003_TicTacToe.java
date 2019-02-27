3d9ed63b3ad633b4dced05996c2d5a3c8918f196
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2019-02-27 01:21:09 -0600

    Add files via upload

---
+++ Problem Set 0003/C0003_TicTacToe.java
@@ -0,0 +1,126 @@
+/*
+
+C - Tic Tac Toe
+
+Certainly, everyone is familiar with tic-tac-toe game. The rules are very simple indeed. Two players take 
+turns marking the cells in a 3 x 3 grid (one player always draws crosses, the other — noughts). The player 
+who succeeds first in placing three of his marks in a horizontal, vertical or diagonal line wins, and the 
+game is finished. The player who draws crosses goes first. If the grid is filled, but neither Xs, nor 0s 
+form the required line, a draw is announced. You are given a 3 x 3 grid, each grid cell is empty, or 
+occupied by a cross or a nought. You have to find the player (first or second), whose turn is next, or 
+print one of the verdicts below:
+    (1) illegal — if the given board layout can't appear during a valid game;
+    (2) the first player won — if in the given board layout the first player has just won;
+    (3) the second player won — if in the given board layout the second player has just won;
+    (4) draw — if the given board layout has just let to a draw. 
+    
+Input
+The input consists of three lines, each of the lines contains characters ".", "X" or "0" (a period, a 
+capital letter X, or a digit zero).
+
+Output
+Print one of the six verdicts: first, second, illegal, the first player won, the second player won or draw.
+
+----------------------------------------------------------------------------------------------------------*/
+
+import java.util.Scanner;
+public class C0003_TicTacToe {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		char[][] board = new char[3][3];
+		
+		int numX = 0, numY = 0;
+		for (int i=0; i<3; i++) {
+			String str = sc.next();
+			for (int j=0; j<3; j++) {
+				char cur = str.charAt(j);
+				if (cur == 'X') 
+					numX++;
+				if (cur == '0') 
+					numY++;
+				board[i][j] = cur;
+			}
+		}
+		sc.close();
+		
+		String res = findAns(numX, numY, board);
+		System.out.println(res);
+	}
+	
+	public static String findAns(int numX, int numY, char[][] board) {
+		if (Math.abs(numX-numY) > 1 || numY > numX)
+			return "illegal";
+		
+		String str = checkSides(numX, numY, board);
+		if (!str.equals(""))
+			return str;
+		
+		if (numX + numY == 9)
+			return "draw";
+		
+		if (numX > numY)
+			return "second";
+		
+		return "first";
+	}
+	
+	public static String checkSides(int numX, int numY, char[][] board) {
+		int[][] arr = new int[3][3];
+		
+		for (int i=0; i<3; i++) {
+			if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+				if (board[i][0] != '.') {
+					arr[i][0]++; arr[i][1]++; arr[i][2]++;
+				}
+			}
+		}
+		for (int i=0; i<3; i++) {
+			if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+				if (board[0][i] != '.') {
+					arr[0][i]++; arr[1][i]++; arr[2][i]++;
+				}
+			}
+		}
+		if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+			if (board[1][1] != '.') {
+				arr[0][0]++; arr[1][1]++; arr[2][2]++;
+			}
+		}
+		if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+			if (board[1][1] != '.') {
+				arr[0][2]++; arr[1][1]++; arr[2][0]++;
+			}
+		}
+		
+		int winSpots = 0, twoWins = 0;
+		for (int i=0; i<3; i++) {
+			for (int j=0; j<3; j++) {
+				if (arr[i][j] > 0)
+					winSpots++;
+				if (arr[i][j] > 1)
+					twoWins++;
+			}
+		}
+		
+		if (twoWins > 1 || winSpots > 5)
+			return "illegal";
+		for (int i=0; i<3; i++) {
+			for (int j=0; j<3; j++) {
+				if (arr[i][j] > 0) {
+					if (board[i][j] == 'X') {
+						if (numX <= numY)
+							return "illegal";
+						return "the first player won";
+					}
+					else {
+						if (numX > numY)
+							return "illegal";
+						return "the second player won";
+					}
+				}
+			}
+		}
+		return "";
+	}
+}
