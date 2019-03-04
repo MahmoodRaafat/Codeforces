7e03d86452d1ab4197e77585958a0933276a82ef
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2019-03-03 23:13:45 -0600

    Math

--- Problem Set 1102/A - Integer Sequence Dividing.java
+++ Problem Set 1102/A - Integer Sequence Dividing.java
@@ -2,7 +2,7 @@
 
 A - Integer Sequence Dividing
 
-You are given an integer sequence 1,2,…,n. You have to divide it into two sets A and B 
+You are given an integer sequence 1,2,â€¦,n. You have to divide it into two sets A and B 
 in such a way that each element belongs to exactly one set and |sum(A)-sum(B)| is the
 minimum possible. The value |x| is the absolute value of x and sum(S) is the sum of 
 elements of the set S.
@@ -11,7 +11,7 @@ Input
 The first line of the input contains one integer n (1 <= n <= 2*10^9).
 
 Output
-Print one integer — the minimum possible value of |sum(A)-sum(B)| if you divide the 
+Print one integer â€” the minimum possible value of |sum(A)-sum(B)| if you divide the 
 initial sequence 1,2, ...,n into two sets A and B.
 
 --------------------------------------------------------------------------------------*/
