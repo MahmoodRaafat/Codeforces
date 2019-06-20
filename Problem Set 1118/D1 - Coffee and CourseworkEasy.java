54abef0a766f62316f296df477f60adab9df05a2
brandon71901 <43732371+brandon71901@users.noreply.github.com>
2019-06-20 00:56:19 -0500

    Brute Force, Greedy

--- Problem Set 1118/D1 - Coffee and CourseworkEasy.java
+++ Problem Set 1118/D1 - Coffee and CourseworkEasy.java
@@ -17,7 +17,7 @@ possible (spend the minimum number of days to do it). Your task is to find out t
 or say that it is impossible.
 
 Input
-The first line of the input contains two integers n and m (1 < n < 100, 1 <= m <= 10^4) — the 
+The first line of the input contains two integers n and m (1 < n < 100, 1 <= m <= 10^4) â€” the 
 number of cups of coffee and the number of pages in the coursework. The second line of the input 
 contains n integers a1, a2, ..., an (1 <= ai <= 100), where ai is the caffeine dosage of coffee 
 in the i-th cup.
